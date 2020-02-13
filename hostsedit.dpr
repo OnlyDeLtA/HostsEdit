20aa78ab0367b737a9dc09aaa0435b441a1bbb3c
OnlyDeLtA <quadxtech@live.com>
2020-02-13 10:54:07 +0530

    Updated to v1.5

--- hostsedit.dpr
+++ hostsedit.dpr
@@ -4,7 +4,8 @@ program hostsedit;
 {$R *.res}
 
 uses
-  System.SysUtils, Winapi.Windows, System.StrUtils, System.RegularExpressions;
+  System.SysUtils, Winapi.Windows, System.Types, System.Classes,
+  System.StrUtils, System.RegularExpressions;
 
 var
   host: string;
@@ -44,58 +45,85 @@ begin
   Result := n;
 end;
 
-procedure Add(site: String);
+procedure Add(ip: String; domain: String);
 var
-  edit: TextFile;
-  hsites: array of string;
+  rstrm: TStreamReader;
+  wstrm: TStreamWriter;
+  hip, hdomain, osites: array of string;
   I, L, n: Integer;
+  splt: TStringDynArray;
   str: string;
+  exists: Boolean;
 begin
+  exists := False;
   RegularExpression.Create(regex1);
   I := 0;
   L := 0;
-  AssignFile(edit, host);
-  Reset(edit);
-  while not Eof(edit) do
+  rstrm := TStreamReader.Create(host);
+  while not(rstrm.endofstream) do
   begin
     inc(L, 1);
-    Readln(edit, str);
+    inc(I, 1);
+    SetLength(hip, I);
+    SetLength(hdomain, I);
+    SetLength(osites, I);
+    str := rstrm.ReadLine;
+    osites[I - 1] := str;
     Match := RegularExpression.Match(str);
     if Match.Success then
     begin
-      inc(I, 1);
       str := Match.Value;
-      SetLength(hsites, I);
       str := TRegEx.Replace(str, regex2, '');
       str := TRegEx.Replace(str, regex3, ' ');
-      hsites[I - 1] := str;
+      splt := Splitstring(str, ' ');
+      hip[I - 1] := splt[0];
+      hdomain[I - 1] := splt[1];
+      if (CompareText(hdomain[I - 1], domain) = 0) and (exists = False) then
+      begin
+        exists := True;
+        osites[I - 1] := ip + ' ' + domain;
+
+      end
+      else if (CompareText(hdomain[I - 1], domain) = 0) and (exists = True) then
+      begin
+        hdomain[I - 1] := 'removed';
+        hip[I - 1] := 'removed';
+      end;
+      Continue;
     end;
+    hip[I - 1] := '';
+    hdomain[I - 1] := '';
   end;
-  CloseFile(edit);
+  rstrm.free;
   n := NewLines;
-  AssignFile(edit, host);
-  Append(edit);
+  wstrm := TStreamWriter.Create(host);
   if n <> L then
-    Writeln(edit);
-  if CheckStr(site, hsites) then
-    Writeln(edit, site);
-  CloseFile(edit);
+    wstrm.writeline;
+
+  for I := Low(hdomain) to High(hdomain) do
+    if (hdomain[I] <> 'removed') then
+      wstrm.writeline(osites[I]);
+
+  if not exists then
+    wstrm.writeline(ip + ' ' + domain);
+
+  wstrm.free;
 end;
 
 procedure Remove(site: string);
 var
   hsites, osites: array of string;
-  edit: TextFile;
+  rstrm: TStreamReader;
+  wstrm: TStreamWriter;
   I: Integer;
   str: string;
 begin
   RegularExpression.Create(regex1);
   I := 0;
-  AssignFile(edit, host);
-  Reset(edit);
-  while not Eof(edit) do
+  rstrm := TStreamReader.Create(host);
+  while not(rstrm.endofstream) do
   begin
-    Readln(edit, str);
+    str := rstrm.ReadLine;
     Match := RegularExpression.Match(str);
     inc(I, 1);
     SetLength(hsites, I);
@@ -104,110 +132,164 @@ begin
     if Match.Success then
     begin
       str := Match.Value;
-      str := TRegEx.Replace(str, regex2, '');
-      str := TRegEx.Replace(str, regex3, ' ');
+      str := TRegEx.Replace(str,
+        '^\h*\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+', '');
       hsites[I - 1] := str;
       Continue;
     end;
     hsites[I - 1] := str;
   end;
-  CloseFile(edit);
-  AssignFile(edit, host);
-  Rewrite(edit);
+  rstrm.free;
+  wstrm := TStreamWriter.Create(host);
   for I := Low(hsites) to High(hsites) do
     if CompareText(hsites[I], site) <> 0 then
-      Writeln(edit, osites[I]);
-  CloseFile(edit);
+      wstrm.writeline(osites[I]);
+  wstrm.free;
 end;
 
 procedure AddFromText(lhost: String);
 var
-  txtfile, edit: TextFile;
-  sites, hsites: array of string;
+  rstrm: TStreamReader;
+  wstrm: TStreamWriter;
+  splt: TStringDynArray;
+  ip, domain, hip, hdomain, osites, aip, adomain: array of string;
   str: string;
-  I, L, n: Integer;
+  I, L, J, K, n: Integer;
+  exists: Boolean;
 begin
   RegularExpression.Create(regex1);
   I := 0;
   L := 0;
-  AssignFile(txtfile, lhost);
-  Reset(txtfile);
-  while not Eof(txtfile) do
-  begin
-    Readln(txtfile, str);
-    Match := RegularExpression.Match(str);
-    if Match.Success then
-    begin
-      inc(I, 1);
-      SetLength(sites, I);
-      str := Match.Value;
-      str := TRegEx.Replace(str, regex2, '');
-      str := TRegEx.Replace(str, regex3, ' ');
-      sites[I - 1] := str;
-    end;
-  end;
-  CloseFile(txtfile);
-  I := 0;
-  AssignFile(edit, host);
-  Reset(edit);
-  while not Eof(edit) do
+  K := 0;
+  rstrm := TStreamReader.Create(host);
+  while not(rstrm.endofstream) do
   begin
     inc(L, 1);
-    Readln(edit, str);
+    inc(I, 1);
+    SetLength(hip, I);
+    SetLength(hdomain, I);
+    SetLength(osites, I);
+    str := rstrm.ReadLine;
+    osites[I - 1] := str;
+    Match := RegularExpression.Match(str);
+    if Match.Success then
+    begin
+      str := Match.Value;
+      str := TRegEx.Replace(str, regex2, '');
+      str := TRegEx.Replace(str, regex3, ' ');
+      splt := Splitstring(str, ' ');
+      hip[I - 1] := splt[0];
+      hdomain[I - 1] := splt[1];
+      Continue;
+    end;
+    hip[I - 1] := '';
+    hdomain[I - 1] := '';
+  end;
+  rstrm.free;
+  I := 0;
+  rstrm := TStreamReader.Create(lhost);
+  while not(rstrm.endofstream) do
+  begin
+    str := rstrm.ReadLine;
     Match := RegularExpression.Match(str);
     if Match.Success then
     begin
       inc(I, 1);
-      SetLength(hsites, I);
+      SetLength(ip, I);
+      SetLength(domain, I);
       str := Match.Value;
       str := TRegEx.Replace(str, regex2, '');
       str := TRegEx.Replace(str, regex3, ' ');
-      hsites[I - 1] := str;
+      splt := Splitstring(str, ' ');
+      ip[I - 1] := splt[0];
+      domain[I - 1] := splt[1];
     end;
   end;
-  CloseFile(edit);
+  rstrm.free;
+  for I := Low(domain) to High(domain) do
+  begin
+    exists := False;
+    for J := Low(hdomain) to High(hdomain) do
+    begin
+      if (CompareText(hdomain[J], domain[I]) = 0) and (exists = False) then
+      begin
+        exists := True;
+        osites[J] := ip[I] + ' ' + domain[I];
+
+      end
+      else if (CompareText(hdomain[J], domain[I]) = 0) and (exists = True) then
+      begin
+        hdomain[J] := 'removed';
+        hip[J] := 'removed';
+      end;
+
+    end;
+
+    if not exists then
+    begin
+      inc(K, 1);
+      SetLength(aip, K);
+      SetLength(adomain, K);
+      aip[K - 1] := ip[I];
+      adomain[K - 1] := domain[I];
+    end;
+  end;
+
   n := NewLines;
-  AssignFile(edit, host);
-  Append(edit);
+  wstrm := TStreamWriter.Create(host);
   if n <> L then
-    Writeln(edit);
-  for I := Low(sites) to High(sites) do
-    if CheckStr(sites[I], hsites) then
-      Writeln(edit, sites[I]);
-  CloseFile(edit);
+    wstrm.writeline;
+  for I := Low(hdomain) to High(hdomain) do
+    if (hdomain[I] <> 'removed') then
+      wstrm.writeline(osites[I]);
+  for I := Low(adomain) to High(adomain) do
+    wstrm.writeline(aip[I] + ' ' + adomain[I]);
+  wstrm.free;
 end;
 
 procedure RemoveFromText(lhost: String);
 var
   sites, hsites, osites: array of string;
-  txtfile, edit: TextFile;
+  rstrm: TStreamReader;
+  wstrm: TStreamWriter;
+  AltRegularExpression: TRegEx;
+  AltMatch: TMatch;
   str: string;
   I: Integer;
 begin
   RegularExpression.Create(regex1);
+  AltRegularExpression.Create('^\h*[^#\s]*');
   I := 0;
-  AssignFile(txtfile, lhost);
-  Reset(txtfile);
-  while not Eof(txtfile) do
+  rstrm := TStreamReader.Create(lhost);
+  while not(rstrm.endofstream) do
   begin
-    Readln(txtfile, str);
+    str := rstrm.ReadLine;
     Match := RegularExpression.Match(str);
+    AltMatch := AltRegularExpression.Match(str);
     if Match.Success then
     begin
       inc(I, 1);
       SetLength(sites, I);
       str := Match.Value;
-      str := TRegEx.Replace(str, regex2, '');
-      str := TRegEx.Replace(str, regex3, ' ');
+      str := TRegEx.Replace(str,
+        '^\h*\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+', '');
+      sites[I - 1] := str;
+    end
+    else if AltMatch.Success then
+    begin
+      inc(I, 1);
+      SetLength(sites, I);
+      str := AltMatch.Value;
+      str := TRegEx.Replace(str, '^\h*', '');
       sites[I - 1] := str;
     end;
   end;
+  rstrm.free;
   I := 0;
-  AssignFile(edit, host);
-  Reset(edit);
-  while not Eof(edit) do
+  rstrm := TStreamReader.Create(host);
+  while not(rstrm.endofstream) do
   begin
-    Readln(edit, str);
+    str := rstrm.ReadLine;
     Match := RegularExpression.Match(str);
     inc(I, 1);
     SetLength(hsites, I);
@@ -216,109 +298,107 @@ begin
     if Match.Success then
     begin
       str := Match.Value;
-      str := TRegEx.Replace(str, regex2, '');
-      str := TRegEx.Replace(str, regex3, ' ');
+      str := TRegEx.Replace(str,
+        '^\h*\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+', '');
       hsites[I - 1] := str;
       Continue;
     end;
     hsites[I - 1] := str;
   end;
-  CloseFile(edit);
-  AssignFile(edit, host);
-  Rewrite(edit);
+  rstrm.free;
+  wstrm := TStreamWriter.Create(host);
   for I := Low(hsites) to High(hsites) do
     if CheckStr(hsites[I], sites) then
-      Writeln(edit, osites[I]);
-  CloseFile(edit);
+      wstrm.writeline(osites[I]);
+  wstrm.free;
 end;
 
 procedure Backup(loc: String);
 var
   hsites: array of string;
-  edit: TextFile;
+  rstrm: TStreamReader;
+  wstrm: TStreamWriter;
   I: Integer;
 begin
   I := 0;
-  AssignFile(edit, host);
-  Reset(edit);
-  while not Eof(edit) do
+  rstrm := TStreamReader.Create(host);
+  while not(rstrm.endofstream) do
   begin
     inc(I, 1);
     SetLength(hsites, I);
-    Readln(edit, hsites[I - 1]);
+    hsites[I - 1] := rstrm.ReadLine;
   end;
 
-  CloseFile(edit);
-  if not DirectoryExists(ExtractFileDir(loc)) then
+  rstrm.free;
+  if (not DirectoryExists(ExtractFileDir(loc))) and
+    (not ExtractFileDir(loc).IsEmpty) then
     ForceDirectories(ExtractFileDir(loc));
 
-  AssignFile(edit, loc);
-  Rewrite(edit);
+  wstrm := TStreamWriter.Create(loc);
   for I := Low(hsites) to High(hsites) do
-    Writeln(edit, hsites[I]);
-  CloseFile(edit);
+    wstrm.writeline(hsites[I]);
+  wstrm.free;
 end;
 
 procedure Restore(loc: String = '');
 var
-  edit: TextFile;
+  rstrm: TStreamReader;
+  wstrm: TStreamWriter;
   sites: array of string;
   I: Integer;
 begin
   if loc = '' then
   begin
-    AssignFile(edit, host);
-    Rewrite(edit);
-    Writeln(edit, '# Copyright (c) 1993-2009 Microsoft Corp.');
-    Writeln(edit, '#');
-    Writeln(edit,
-      '# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.');
-    Writeln(edit, '#');
-    Writeln(edit,
-      '# This file contains the mappings of IP addresses to host names. Each');
-    Writeln(edit,
-      '# entry should be kept on an individual line. The IP address should');
-    Writeln(edit,
-      '# be placed in the first column followed by the corresponding host name.');
-    Writeln(edit,
-      '# The IP address and the host name should be separated by at least one');
-    Writeln(edit, '# space.');
-    Writeln(edit, '#');
-    Writeln(edit,
-      '# Additionally, comments (such as these) may be inserted on individual');
-    Writeln(edit,
-      '# lines or following the machine name denoted by a ''#'' symbol.');
-    Writeln(edit, '#');
-    Writeln(edit, '# For example:');
-    Writeln(edit, '#');
-    Writeln(edit,
-      '#      102.54.94.97     rhino.acme.com          # source server');
-    Writeln(edit,
-      '#       38.25.63.10     x.acme.com              # x client host');
-    Writeln(edit, '');
-    Writeln(edit, '# localhost name resolution is handled within DNS itself.');
-    Writeln(edit, '#	127.0.0.1       localhost');
-    Writeln(edit, '#	::1             localhost');
-    CloseFile(edit);
+    wstrm := TStreamWriter.Create(host);
+    wstrm.writeline('# Copyright (c) 1993-2009 Microsoft Corp.');
+    wstrm.writeline('#');
+    wstrm.writeline
+      ('# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.');
+    wstrm.writeline('#');
+    wstrm.writeline
+      ('# This file contains the mappings of IP addresses to host names. Each');
+    wstrm.writeline
+      ('# entry should be kept on an individual line. The IP address should');
+    wstrm.writeline
+      ('# be placed in the first column followed by the corresponding host name.');
+    wstrm.writeline
+      ('# The IP address and the host name should be separated by at least one');
+    wstrm.writeline('# space.');
+    wstrm.writeline('#');
+    wstrm.writeline
+      ('# Additionally, comments (such as these) may be inserted on individual');
+    wstrm.writeline
+      ('# lines or following the machine name denoted by a ''#'' symbol.');
+    wstrm.writeline('#');
+    wstrm.writeline('# For example:');
+    wstrm.writeline('#');
+    wstrm.writeline
+      ('#      102.54.94.97     rhino.acme.com          # source server');
+    wstrm.writeline
+      ('#       38.25.63.10     x.acme.com              # x client host');
+    wstrm.writeline('');
+    wstrm.writeline
+      ('# localhost name resolution is handled within DNS itself.');
+    wstrm.writeline('#	127.0.0.1       localhost');
+    wstrm.writeline('#	::1             localhost');
+    wstrm.free;
   end
   else
   begin
     I := 0;
-    AssignFile(edit, loc);
-    Reset(edit);
-    while not Eof(edit) do
+    rstrm := TStreamReader.Create(loc);
+    while not(rstrm.endofstream) do
     begin
       inc(I, 1);
       SetLength(sites, I);
-      Readln(edit, sites[I - 1]);
+      sites[I - 1] := rstrm.ReadLine;
     end;
-    CloseFile(edit);
+    rstrm.free;
 
-    AssignFile(edit, host);
-    Rewrite(edit);
+    wstrm := TStreamWriter.Create(host);
     for I := Low(sites) to High(sites) do
-      Writeln(edit, sites[I]);
-    CloseFile(edit);
+      wstrm.writeline(sites[I]);
+    wstrm.free;
   end;
 end;
 
@@ -337,25 +417,25 @@ begin
     host := GetEnvironmentVariable('WINDIR') + '\System32\drivers\etc\hosts';
     if ParamCount <> 0 then
     begin
-      if ParamStr(1) = '/a' then
+      if (ParamStr(1) = '/a') and (ParamCount = 3) then
       begin
         Writeln('Adding entry to HOSTS file...');
         attrib := FileGetAttr(host);
         FileSetAttr(host, faArchive);
-        Add(ParamStr(2) + ' ' + ParamStr(3));
+        Add(ParamStr(2), ParamStr(3));
         FileSetAttr(host, attrib);
         Writeln('Done.');
       end;
-      if ParamStr(1) = '/r' then
+      if (ParamStr(1) = '/r') and (ParamCount = 2) then
       begin
         Writeln('Removing entry from HOSTS file...');
         attrib := FileGetAttr(host);
         FileSetAttr(host, faArchive);
-        Remove(ParamStr(2) + ' ' + ParamStr(3));
+        Remove(ParamStr(2));
         FileSetAttr(host, attrib);
         Writeln('Done.');
       end;
-      if ParamStr(1) = '/am' then
+      if (ParamStr(1) = '/am') and (ParamCount = 2) then
       begin
         Writeln('Adding entries to HOSTS file...');
         attrib := FileGetAttr(host);
@@ -364,7 +444,7 @@ begin
         FileSetAttr(host, attrib);
         Writeln('Done.');
       end;
-      if ParamStr(1) = '/rm' then
+      if (ParamStr(1) = '/rm') and (ParamCount = 2) then
       begin
         Writeln('Removing entries from HOSTS file...');
         attrib := FileGetAttr(host);
@@ -373,23 +453,28 @@ begin
         FileSetAttr(host, attrib);
         Writeln('Done.');
       end;
-      if ParamStr(1) = '/b' then
+      if (ParamStr(1) = '/b') and (ParamCount = 2) then
       begin
         Writeln('Creating HOSTS file backup...');
         Backup(ParamStr(2));
         Writeln('Done.');
       end;
-      if ParamStr(1) = '/res' then
+      if (ParamStr(1) = '/res') and (ParamCount = 2) and FileExists(ParamStr(2))
+      then
       begin
         Writeln('Restoring HOSTS file...');
         FileSetAttr(host, faArchive);
-        if ParamCount > 1 then
-          Restore(ParamStr(2))
-        else
-          Restore;
+        Restore(ParamStr(2));
         Writeln('Done.');
       end;
-      if ParamStr(1) = '/attr' then
+      if (ParamStr(1) = '/res') and (ParamCount = 1) then
+      begin
+        Writeln('Restoring HOSTS file...');
+        FileSetAttr(host, faArchive);
+        Restore;
+        Writeln('Done.');
+      end;
+      if (ParamStr(1) = '/attr') and (ParamCount = 2) then
       begin
         Writeln('Changing Attributes for HOSTS file...');
         SetAttrib(ParamStr(2));
@@ -398,7 +483,7 @@ begin
     end
     else
     begin
-      SetConsoleTitle('hostsedit 1.4');
+      SetConsoleTitle('hostsedit 1.5');
       Writeln('Command line utility for editing Windows HOSTS file.');
       Writeln('Freeware');
       Writeln('');
@@ -415,7 +500,7 @@ begin
       Writeln('Samples :');
       Writeln('');
       Writeln('  hostsedit /a 0.0.0.0 www.example-domain.com');
-      Writeln('  hostsedit /r 0.0.0.0 www.example-domain.com ');
+      Writeln('  hostsedit /r www.example-domain.com');
       Writeln('  hostsedit /am "D:\HOSTS Entries\example.txt"');
       Writeln('  hostsedit /rm "D:\HOSTS Entries\example.txt"');
       Writeln('  hostsedit /b "D:\HOSTS.BKP"');
